67581126222a22dcf089781f47ddad9eb47534fa
rickie <rickiemaweu@gmail.com>
2022-08-18 01:41:28 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize (word)
+        @word = word
+    end
+    def match(arr)
+        matched = []
+        arr.map do |word|
+            if word.chars.sort == @word.chars.sort
+              matched.push(word)
+            
+            end
+        end
+        matched
+    end
+end
